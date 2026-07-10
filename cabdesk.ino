23d88e26a526cd428b19a980d2ee99b0054038c7
Paál Donát <paal.donat@gmail.com>
2026-07-10 15:18:25 +0200

    Write main .ino file

--- cabdesk.ino
+++ cabdesk.ino
@@ -1,8 +1,31 @@
+#include "LeverButton.h"
+#include "LeverAxis.h"
+#include "GamepadManager.h"
+#include "DoorButtons.h"
+#include "PinConfig.h"
+
+GamepadManager _gamepadManager;
+
+LeverAxis _leverAxis(&_gamepadManager);
+LeverButton _leverButton(&_gamepadManager);
+DoorButtons _doorButtons(&_gamepadManager);
+
 void setup() {
-  // put your setup code here, to run once:
+  PinConfig::initPins();
+
+  _leverAxis.begin();
+  _leverButton.begin();
+  _doorButtons.begin();
+
+  _gamepadManager.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  _leverAxis.update();
+  _leverButton.update();
+  _doorButtons.update();
 
+  _gamepadManager.send();
+
+  delay(20);
 }
