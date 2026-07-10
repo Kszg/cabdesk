f211d0f52b3468413ff7016107c32e336fb440b2
Paál Donát <paal.donat@gmail.com>
2026-07-10 18:14:12 +0200

    Fill out lever config values
    For ÖBB 4024 (Talent 1) in TSW 6

--- LeverConfig.h
+++ LeverConfig.h
@@ -1,18 +1,18 @@
 #pragma once
 
 namespace LeverConfig {
-  constexpr uint16_t POT_TRACTION_MIN = 0;
-  constexpr uint16_t POT_TRACTION_MAX = 0;
-  constexpr uint16_t POT_BRAKING_MAX = 0;
-  constexpr uint16_t POT_BRAKING_MIN = 0;
-  constexpr uint16_t POT_EBRAKE_MIN = 0;
+  constexpr uint16_t POT_TRACTION_MIN = 545;
+  constexpr uint16_t POT_TRACTION_MAX = 750;
+  constexpr uint16_t POT_BRAKING_MAX = 512;
+  constexpr uint16_t POT_BRAKING_MIN = 335;
+  constexpr uint16_t POT_EBRAKE_MIN = 320;
 
-  constexpr int8_t AXIS_TRACTION_MIN = 0;
-  constexpr int8_t AXIS_TRACTION_MAX = 0;
+  constexpr int8_t AXIS_TRACTION_MIN = 15;
+  constexpr int8_t AXIS_TRACTION_MAX = 100;
   constexpr int8_t AXIS_NEUTRAL_VALUE = 0;
-  constexpr int8_t AXIS_BRAKING_MAX = 0;
-  constexpr int8_t AXIS_BRAKING_MIN = 0;
-  constexpr int8_t AXIS_EBRAKE_VALUE = 0;
+  constexpr int8_t AXIS_BRAKING_MAX = -5;
+  constexpr int8_t AXIS_BRAKING_MIN = -79;
+  constexpr int8_t AXIS_EBRAKE_VALUE = -100;
 
   constexpr int8_t AXIS_MIN = -100;
   constexpr int8_t AXIS_MAX = 100;
