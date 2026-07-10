55eb50cdcf03357fb8bc0b6394c9732edca7d09b
paalzoltanweb <paal.donat@gmail.com>
2026-07-10 14:45:44 +0200

    Door buttons class

---
+++ DoorButtons.cpp
@@ -0,0 +1,41 @@
+#include "DoorButtons.h"
+
+GamepadManager::GamepadManager(GamepadManager* gamepadManagerRef) {
+  _gamepadManager = gamepadManagerRef;
+}
+
+void DoorButtons::begin() {
+
+}
+
+bool _closePressedLastFrame = false;
+
+void DoorButtons::update() {
+  bool leftPressed = (digitalRead(PinConfig::DOOR_L) == LOW);
+  bool closePressed = (digitalRead(PinConfig::DOOR_CLOSE) == LOW);
+  bool rightPressed = (digitalRead(PinConfig::DOOR_R) == LOW);
+
+  if (closePressed && (_leftOpen || _rightOpen || _closePressedLastFrame)) {
+    _gamepadManager->doorsL = leftPressed;
+    _gamepadManager->doorsR = rightPressed;
+
+    _leftOpen = false;
+    _rightOpen = false;
+  }
+  else {
+    _gamepadManager->doorsL = false;
+    _gamepadManager->doorsR = false;
+
+    if (leftPressed) {
+      _gamepadManager->doorsL = true;
+      _leftOpen = true;
+    }
+
+    if (rightPressed) {
+      _gamepadManager->doorsR = true;
+      _leftOpen = true;
+    }
+  }
+
+  _closePressedLastFrame = closePressed;
+}
---
+++ DoorButtons.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <Arduino.h>
+#include "PinConfig.h"
+#include "GamepadManager.h"
+
+class DoorButtons {
+  private:
+    GamepadManager* _gamepadManager;
+
+    bool _leftOpen;
+    bool _rightOpen;
+
+  public:
+    DoorButtons(GamepadManager* gamepadManagerRef);
+
+    void begin();
+    void update();
+}
