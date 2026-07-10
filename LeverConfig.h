e05824e55af3ebee73311c56a7fa0865aa5f4cca
paalzoltanweb <paal.donat@gmail.com>
2026-07-10 13:39:09 +0200

    Lever config naming convention improvements

--- LeverConfig.h
+++ LeverConfig.h
@@ -1,15 +1,16 @@
 #pragma once
 
 namespace LeverConfig {
-  constexpr uint16_t TRACTION_POT_MIN = 0;
-  constexpr uint16_t TRACTION_POT_MAX = 0;
-  constexpr uint16_t BRAKING_POT_MAX = 0;
-  constexpr uint16_t BRAKING_POT_MIN = 0;
-  constexpr uint16_t EBRAKE_POT_MIN = 0;
+  constexpr uint16_t POT_TRACTION_MIN = 0;
+  constexpr uint16_t POT_TRACTION_MAX = 0;
+  constexpr uint16_t POT_BRAKING_MAX = 0;
+  constexpr uint16_t POT_BRAKING_MIN = 0;
+  constexpr uint16_t POT_EBRAKE_MIN = 0;
 
-  constexpr int8_t TRACTION_AXIS_MIN = 0;
-  constexpr int8_t TRACTION_AXIS_MAX = 0;
-  constexpr int8_t BRAKING_AXIS_MAX = 0;
-  constexpr int8_t BRAKING_AXIS_MIN = 0;
-  constexpr int8_t EBRAKE_AXIS_MIN = 0;
+  constexpr int8_t AXIS_TRACTION_MIN = 0;
+  constexpr int8_t AXIS_TRACTION_MAX = 0;
+  constexpr int8_t AXIS_NEUTRAL_VALUE = 0;
+  constexpr int8_t AXIS_BRAKING_MAX = 0;
+  constexpr int8_t AXIS_BRAKING_MIN = 0;
+  constexpr int8_t AXIS_EBRAKE_VALUE = 0;
 }
