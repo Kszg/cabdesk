a9e011c1d7e0821adb41abba9f65d62432ee3e91
paalzoltanweb <paal.donat@gmail.com>
2026-07-10 13:05:12 +0200

    Lever config

--- LeverConfig.h
+++ LeverConfig.h
@@ -1,5 +1,15 @@
 #pragma once
 
 namespace LeverConfig {
-  constexpr 
+  constexpr uint16_t TRACTION_POT_MIN = 0;
+  constexpr uint16_t TRACTION_POT_MAX = 0;
+  constexpr uint16_t BRAKING_POT_MAX = 0;
+  constexpr uint16_t BRAKING_POT_MIN = 0;
+  constexpr uint16_t EBRAKE_POT_MIN = 0;
+
+  constexpr int8_t TRACTION_AXIS_MIN = 0;
+  constexpr int8_t TRACTION_AXIS_MAX = 0;
+  constexpr int8_t BRAKING_AXIS_MAX = 0;
+  constexpr int8_t BRAKING_AXIS_MIN = 0;
+  constexpr int8_t EBRAKE_AXIS_MIN = 0;
 }
