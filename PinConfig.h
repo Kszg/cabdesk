eee89fdb9ecd98f13dbe2cf20598d5032a82eec6
Paál Donát <paal.donat@gmail.com>
2026-07-10 15:28:27 +0200

    Fill out pin config

--- PinConfig.h
+++ PinConfig.h
@@ -4,10 +4,10 @@
 
 namespace PinConfig {
   constexpr uint8_t LEVER_POT = 0;
-  constexpr uint8_t LEVER_BUTTON = 0;
-  constexpr uint8_t DOOR_L = 0;
-  constexpr uint8_t DOOR_CLOSE = 0;
-  constexpr uint8_t DOOR_R = 0;
+  constexpr uint8_t LEVER_BUTTON = 13;
+  constexpr uint8_t DOOR_L = 12;
+  constexpr uint8_t DOOR_CLOSE = 11;
+  constexpr uint8_t DOOR_R = 10;
 
   void initPins();
 }
