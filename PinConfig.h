a9b4d738be98459ca6d88a9b837e99f8c0a5da3b
paalzoltanweb <paal.donat@gmail.com>
2026-07-10 08:55:54 +0200

    Pin config file

---
+++ PinConfig.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace PinConfig {
+  constexpr uint8_t LEVER_POT = 0;
+  constexpr uint8_t LEVER_BUTTON = 0;
+  constexpr uint8_t DOOR_L = 0;
+  constexpr uint8_t DOOR_CLOSE = 0;
+  constexpr uint8_t DOOR_R = 0;
+}
