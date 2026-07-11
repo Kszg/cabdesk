8674a5003dd92064591db958c16373aa8eead08e
Paál Donát <paal.donat@gmail.com>
2026-07-11 19:45:44 +0200

    Logic fixes in lever axis code

--- LeverAxis.cpp
+++ LeverAxis.cpp
@@ -10,7 +10,7 @@ void LeverAxis::begin() {
 
 void LeverAxis::update() {
   uint16_t potValue = analogRead(PinConfig::LEVER_POT);
-  int8_t axisValue = 0;
+  int8_t axisValue = LeverConfig::AXIS_NEUTRAL_VALUE;
   
   bool traction = (potValue >= LeverConfig::POT_TRACTION_MIN);
   bool leverButtonPressed = (digitalRead(PinConfig::LEVER_BUTTON) == LOW);
@@ -21,7 +21,7 @@ void LeverAxis::update() {
   else if (potValue >= LeverConfig::POT_TRACTION_MIN) { // Traction
     axisValue = map(potValue, LeverConfig::POT_TRACTION_MIN, LeverConfig::POT_TRACTION_MAX, LeverConfig::AXIS_TRACTION_MIN, LeverConfig::AXIS_TRACTION_MAX);
   }
-  else { // Braking
+  else if (potValue <= LeverConfig::POT_BRAKING_MAX) { // Braking
     if (potValue <= LeverConfig::POT_EBRAKE_MIN) axisValue = LeverConfig::AXIS_EBRAKE_VALUE;
     else {
       axisValue = map(potValue, LeverConfig::POT_BRAKING_MIN, LeverConfig::POT_BRAKING_MAX, LeverConfig::AXIS_BRAKING_MIN, LeverConfig::AXIS_BRAKING_MAX);
